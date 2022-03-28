4d0baafc5cb02d444d2f27c7bbbd0aa083081278
Keisuke Ishigami <keisuke.130@icloud.com>
2022-03-28 23:13:15 +0900

    コースが重複して表示される不具合を修正

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -8,122 +8,122 @@ import '@aws-amplify/ui-react/styles.css'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
-const courses = []
-const eng_courses = []
-const information = [
-  {
-    faculty: {
-      japanese: {
-        name: "工学部"
-      },
-      english: {
-        name: "engineering"
-      }
-    },
-    department: [
-      {
-        japanese: {
-          name: "電気電子・情報工学科",
-          course: ["情報コース", "電気電子コース", "応用物理コース"]
-        },
-        english: {
-          name: "electronic_and_computer_engineering",
-          course: ["information_course", "electrical_electronic_course", "applied_physics_course"]
-        },
-      },
-      {
-        japanese: {
-          name: "機械工学科",
-          course: ["機械コース", "知能機械コース"]
-        },
-        english: {
-          name: "mechanical_engineering",
-          course: ["mechanical_engineering_course", "intelligence_mechanical_engineering_course"]
-        }
-      },
-      {
-        japanese: {
-          name: "化学・生命工学科",
-          course: ["物質化学コース", "生命化学コース"]
-        },
-        english: {
-          name: "chemistry_and_biomolecular_science",
-          course: ["materials_chemistry_course", "biomolecular_science_course"]
-        }
-      }
-    ],
-    color: "primary"
-  },
-  {
-    faculty: {
-      japanese: {
-        name: "応用生物科学部"
-      },
-      english: {
-        name: "applied_bbiological_sciences"
-      }
-    },
-    department: [
-      {
-        japanese: {
-          name: "生産環境科学科",
-          course: ["応用植物科学コース", "応用動物科学コース", "環境生態科学コース"]
-        },
-        english: {
-          name: "agricultural_and_environmental_sciences",
-          course: ["plant_production", "animal_science_and_environmental_science", "ecology"]
-        }
-      },
-      {
-        japanese: {
-          name: "応用生命科学科",
-          course: ["分子生命科学コース", "食品生命科学コース"]
-        },
-        english: {
-          name: "food_and_life_sciences",
-          course: ["molecular_life_science", "food_science"]
-        }
-      }
-    ],
-    color: "secondary",
-  },
-  {
-    faculty: {
-      japanese: {
-        name: "地域科学部"
-      },
-      english: {
-        name: "regional_studies"
-      },
-    },
-    department: [
-      {
-        japanese: {
-          name: "地域政策学科",
-          course: []
-        },
-        english: {
-          name: "policy_studies",
-          course: []
-        }
-      },
-      {
-        japanese: {
-          name: "地域文化学科",
-          course: []
-        },
-        english: {
-          name: "cultural_studies",
-          course: []
-        }
-      }
-    ],
-    color: "success"
-  }
-]
-
 const List = ({ user, signOut }: any) => {
+  const courses = []
+  const eng_courses = []
+  const information = [
+    {
+      faculty: {
+        japanese: {
+          name: "工学部"
+        },
+        english: {
+          name: "engineering"
+        }
+      },
+      department: [
+        {
+          japanese: {
+            name: "電気電子・情報工学科",
+            course: ["情報コース", "電気電子コース", "応用物理コース"]
+          },
+          english: {
+            name: "electronic_and_computer_engineering",
+            course: ["information_course", "electrical_electronic_course", "applied_physics_course"]
+          },
+        },
+        {
+          japanese: {
+            name: "機械工学科",
+            course: ["機械コース", "知能機械コース"]
+          },
+          english: {
+            name: "mechanical_engineering",
+            course: ["mechanical_engineering_course", "intelligence_mechanical_engineering_course"]
+          }
+        },
+        {
+          japanese: {
+            name: "化学・生命工学科",
+            course: ["物質化学コース", "生命化学コース"]
+          },
+          english: {
+            name: "chemistry_and_biomolecular_science",
+            course: ["materials_chemistry_course", "biomolecular_science_course"]
+          }
+        }
+      ],
+      color: "primary"
+    },
+    {
+      faculty: {
+        japanese: {
+          name: "応用生物科学部"
+        },
+        english: {
+          name: "applied_bbiological_sciences"
+        }
+      },
+      department: [
+        {
+          japanese: {
+            name: "生産環境科学科",
+            course: ["応用植物科学コース", "応用動物科学コース", "環境生態科学コース"]
+          },
+          english: {
+            name: "agricultural_and_environmental_sciences",
+            course: ["plant_production", "animal_science_and_environmental_science", "ecology"]
+          }
+        },
+        {
+          japanese: {
+            name: "応用生命科学科",
+            course: ["分子生命科学コース", "食品生命科学コース"]
+          },
+          english: {
+            name: "food_and_life_sciences",
+            course: ["molecular_life_science", "food_science"]
+          }
+        }
+      ],
+      color: "secondary",
+    },
+    {
+      faculty: {
+        japanese: {
+          name: "地域科学部"
+        },
+        english: {
+          name: "regional_studies"
+        },
+      },
+      department: [
+        {
+          japanese: {
+            name: "地域政策学科",
+            course: []
+          },
+          english: {
+            name: "policy_studies",
+            course: []
+          }
+        },
+        {
+          japanese: {
+            name: "地域文化学科",
+            course: []
+          },
+          english: {
+            name: "cultural_studies",
+            course: []
+          }
+        }
+      ],
+      color: "success"
+    }
+  ]
   const router = useRouter()
+
   return (
     <div className={styles.container}>
       <Head>
